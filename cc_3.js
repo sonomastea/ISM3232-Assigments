072dcacc7f45e84405a40aa847922b22095dfad3
sonomastea <sbstea@usf.edu>
2024-05-20 15:24:17 -0400

    Add files via upload

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+//Car Class Implementation Challenge
+//Step 1 
+//Implement a car class with a constructor that that initializes the make and speed properties 
+class User{
+    constructor(make,speed){
+    this.make= make;
+    this.speed= speed;
+    }
+}
+
+const userOne = new User(BMW,120 km/h)
+const userTwo = new User(Mercedes,95 km/h)
+ console.log(userOne);
+ console.log(userTwo);
